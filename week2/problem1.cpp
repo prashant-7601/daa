bc07eac7c85f690e9746e14afae724972301d681
prashant-7601 <prashantarya070601@gmail.com>
2021-08-02 22:06:18 +0530

    changes made to problem 1

--- week2/problem1.cpp
+++ week2/problem1.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
@@ -11,10 +11,20 @@ int main(){
         for(int i=0; i<n; i++)
             cin>>arr[i];
         cin>>k;
-        int count = 0;
-        for(int i=0; i<n; i++){
-            if(arr[i] == k)
-                count++;
+        int count = 0, exp=0, index=n-1;
+        if(arr[int(pow(2, exp))]>=k){
+            index=0;
+        }
+        else{
+            while(arr[int(pow(2, exp+1))] < k && int(pow(2, exp+1)) <= n)
+                exp++;
+            index = int(pow(2, exp));
+        }
+        while(arr[index]!=k)
+            index++;
+        while(arr[index]==k){
+            count++;
+            index++;
         }
         if(count>0)
             cout<<k<<" - "<<count<<"\n";
